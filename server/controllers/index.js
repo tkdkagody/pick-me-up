4be3cccfbd196a606e1a4268a26c3682239414bd
Sunryeo <elma9700@gmail.com>
2021-08-26 20:20:52 +0900

    FEAT:GetMyPost.js/FIX:ProfileChange.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 const { changeProfile } = require('../controllers/ProfileChange');
+const { getMyPost } = require("../controllers/GetMyPost");
 const { Router } = require("express");
 const router = Router();
 
 router.patch("/user/profile/:id", changeProfile);
+router.get('/user/posting-list/:id', getMyPost);
 
 router.get("/", (res, req) => {
   res.send("hello world!");
