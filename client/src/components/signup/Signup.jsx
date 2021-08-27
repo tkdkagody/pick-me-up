2fa8903deef3ed973bb7674ddb55c1c39ba08fcd
tkdkagody <tkdkagody@gmail.com>
2021-08-27 09:29:56 +0900

    FEAT/moal

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -2,40 +2,51 @@ import styles from './Signup.module.css';
 
 import React from 'react';
 
-const Signup = (props) => {
+const Signup = ({setIsSignUpClicked, clickCloseSignIn}) => {
+
+      const clickCloseAll = () => {
+            setIsSignUpClicked(false);
+            clickCloseSignIn();
+      }
+
     return(
-        <section className={styles.signin}>
-        <h1 className={styles.title}>Sign up</h1>
+            <section className={styles.backdrop}>
+                  <div className={styles.signup}>
+                  <div className={styles.titlebox}>
+                  <span className={styles.title}>sign up</span>
+                  <span className={styles.close} onClick={clickCloseAll}>
+                        <img src="../../../images/close.svg" className={styles.img}></img>
+                  </span>
+                  </div>
+                  {/* <form className={styles.form}> */}
+                  <ul className={styles.list}>
+                        <li className={styles.item}>
+                              <input type="text"  className={styles.text} placeholder="아이디를 입력하세요"/>
+                              <label className={styles.label}>이미 존재하는 아이디입니다.</label>
+                        </li>
+                        <li className={styles.item}>
+                              <input type="text"  className={styles.text} placeholder="닉네임을 입력하세요"/>
+                        </li>
+                        <li className={styles.item}>
+                              <input type="text"  className={styles.text} placeholder="휴대폰번호를 입력하세요"/>
+                        </li>
+                        <li className={styles.item}>
+                              <input type="text"  className={styles.text} placeholder="비밀번호를 입력하세요"/>
+                        </li>
+                        <li className={styles.item}>
+                              <input type="text"  className={styles.text} placeholder="비밀번호를 확인해주세요"/>
+                              <label className={styles.label}>비밀번호를 틀렸습니다.</label>
+                        </li>
 
-        <ul className={styles.list}>
-           <li className={styles.item}>
-                <input type="text"  className={styles.text} placeholder="아이디를 입력하세요"/>
-                <label className={styles.label}>이미 존재하는 아이디입니다.</label>
-          </li>
-
-          <li className={styles.item}>
-                <input type="text"  className={styles.text} placeholder="닉네임을 입력하세요"/>
-          </li>
-
-          <li className={styles.item}>
-                <input type="text"  className={styles.text} placeholder="휴대폰번호를 입력하세요"/>
-          </li>
-
-          <li className={styles.item}>
-                <input type="text"  className={styles.text} placeholder="비밀번호를 입력하세요"/>
-          </li>
-          <li className={styles.item}>
-                <input type="text"  className={styles.text} placeholder="비밀번호를 확인해주세요"/>
-                <label className={styles.label}>비밀번호가 틀렸습니다. </label>
-          </li>
-          
-          <li className={styles.item}>
-            <button className={styles.signup} >
-            Sign up
-            </button>
-          </li>
-        </ul>
-      </section>
+                        <li className={styles.item}>
+                              <button className={styles.signupBtn} >
+                              Sign up
+                              </button>
+                        </li>
+                  </ul>
+                  {/* </form> */}
+                  </div>
+            </section>
     );
 }
 
