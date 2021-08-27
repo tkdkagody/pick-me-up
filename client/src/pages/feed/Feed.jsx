41447b0b5d98e6428a2afb3e1d649201cf16cad6
tkdkagody <tkdkagody@gmail.com>
2021-08-27 14:44:21 +0900

    FEAT:graph

--- client/src/pages/feed/Feed.jsx
+++ client/src/pages/feed/Feed.jsx
@@ -3,19 +3,28 @@ import Vote from '../../components/vote/Vote';
 import VoteResult from '../../components/voteResult/VoteResult';
 import styles from './Feed.module.css';
 import LoadingIndecator from '../../components/LoadingIndicator';
+import RealVote from '../../components/realVote/RealVote';
 
 
 const Feed = ({feed}) => {
 
   const [isVoted, setIsVoted] = useState(false);
+
+  const [isVoteReal, setIsVoteReal] = useState(false);
+
   const [isLoading, setIsLoading] = useState(false);
 
   const vote = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsVoted(true);
-    }, 1000);
-    setIsLoading(false);
+    // setIsLoading(true);
+    // setTimeout(() => {
+    //   setIsVoted(true);
+    // }, 1000);
+    // setIsLoading(false);
+    setIsVoted(true);
+  }
+
+  const clickYes = ()=> {
+    setIsVoteReal(true)
   }
 
   return (  
@@ -31,10 +40,30 @@ const Feed = ({feed}) => {
         </div>
         <p className={styles.content}>{feed.content}</p>
         <div className={styles.voteText}>{feed.votes}명이 투표했어요</div>
-        {isLoading ? <LoadingIndecator />
+        {
+          isVoted ?
+          (<>
+          <RealVote feed={feed} handleVote={vote} clickYes={clickYes}>
+          </RealVote>
+          <Vote feed={feed} handleVote={vote} isVoted={isVoted}/>
+          </>)
+          :
+          <Vote feed={feed} handleVote={vote} isVoted={isVoted}/>
+        }
+ 
+              {/* {
+                isVoteReal ?
+                <VoteResult />
+                :
+                <Vote feed={feed} handleVote={vote} isVoted={isVoted}/>
+              } */}
+
+
+        {/* <VoteResult /> */}
+        {/* {isLoading ? <LoadingIndecator />
         : null} 
         {isVoted ? <VoteResult/> 
-        : <Vote feed={feed} handleVote={vote}/>}
+        : <Vote feed={feed} handleVote={vote}/>} */}
         </div> 
     </section>
 )
