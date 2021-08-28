e669f4319f921c80a4ce8795e1005d4145ff2f70
lee <xehddnr@naver.com>
2021-08-28 12:48:31 +0900

    fix/script/start.sh

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -13,6 +13,10 @@ router.patch("/user/profile/:id", changeProfile);
 router.post("/sign-up", (req, res) => {
   const { userId, password, userName, mobile, signUpType } = req.body;
   console.log(userId, password, userName, mobile, signUpType);
+
+  if (process.env.ACCESS_SECRET) {
+    return res.status(423).send("good");
+  }
   if (!userId || !password || !userName || !mobile) {
     return res.status(422).send("insufficient parameters supplied");
   }
