387ad795631035e6acaa17d018e87617d61478fc
lee <xehddnr@naver.com>
2021-08-30 16:12:40 +0900

    pull upstream

--- server/models/vote.js
+++ server/models/vote.js
@@ -21,7 +21,7 @@ module.exports = function (sequelize, DataTypes) {
         type: DataTypes.INTEGER,
         allowNull: true,
       },
-      oppostion: {
+      opposition: {
         type: DataTypes.INTEGER,
         allowNull: true,
       },
