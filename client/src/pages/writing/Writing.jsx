c46671f0b18a202c0ed3eb0efd977872be24e1fe
devjade <jiyoung.park98@gmail.com>
2021-08-31 21:01:40 +0900

    FEAT/getuserid

--- client/src/pages/writing/Writing.jsx
+++ client/src/pages/writing/Writing.jsx
@@ -1,50 +1,48 @@
-import React, { useState } from "react";
-import styles from "./Writing.module.css";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import AWS from "aws-sdk";
+import React, { useState } from 'react';
+import styles from './Writing.module.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import AWS from "aws-sdk"
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
-
-const Writing = ({accessToken, isLogin, setListRender}) => {
+const Writing = ({accessToken, isLogin, setWriteDone}) => {
 
   const tags=['#의류', '#리빙', '#뷰티', '#식품', '#잡화', '#디지털']
 
   const [title, setTitle] = useState(''); //제목
   const [firstOpt, setFirstOpt]= useState('')//옵션명1
   const [secondOpt, setSecondOpt]= useState('')//옵션명2
-
   const [firstImg, setFirstImg] = useState(null); //이미지파일
   const [secondImg, setSecondImg] = useState(null); //이미지파일
-  const [content, setContent] = useState(""); // 컨텐츠
+  const [content, setContent] = useState('') // 컨텐츠
   const [isClicked, setClicked] = useState([]); //카테고리 담는 배열
 
   const [errorMessage, setErrorMessage] = useState(null);
-
+  
   const titleHandle = (event) => {
     setTitle(event.target.value);
-  };
+  }
 
   const firstOptHandle = (event) => {
     setFirstOpt(event.target.value);
-  };
+  }
 
   const secondOptHandle = (event) => {
     setSecondOpt(event.target.value);
-  };
+  }
 
   const tagHandle = (tag) => {
-    if (isClicked.includes(tag)) {
-      setClicked(isClicked.filter((el) => !(el === tag)));
-    } else {
-      setClicked(isClicked.concat(tag));
+    if(isClicked.includes(tag)){
+      setClicked(isClicked.filter(el=> !(el===tag)))
+    } else{
+        setClicked(isClicked.concat(tag))
+      }
     }
-  };
 
   const contentHandle = (event) => {
-    setContent(event.target.value);
-  };
+    setContent(event.target.value)
+  }
 
   //////////////////////이미지 핸들러(아래)///////////////////////////////
   AWS.config.update({
@@ -52,11 +50,12 @@ const Writing = ({accessToken, isLogin, setListRender}) => {
     credentials: new AWS.CognitoIdentityCredentials({
       IdentityPoolId: "ap-northeast-2:a17da5be-96ef-4046-aaa8-62455cef2362", // cognito 인증 풀에서 받아온 키를 문자열로 입력합니다. (Ex. "ap-northeast-2...")
     }),
-  });
+  })
 
-  const firstImgHandle = (event) => {
-    const imageFile = event.target.files[0];
-    if (!imageFile) {
+  const firstImgHandle = (event) => { 
+    
+    const imageFile = event.target.files[0]
+    if(!imageFile){
       return setFirstImg(null);
     }
 
@@ -66,9 +65,9 @@ const Writing = ({accessToken, isLogin, setListRender}) => {
         Key: imageFile.name,
         Body: imageFile,
       },
-    });
-
-    const promise = upload.promise();
+    })
+    
+    const promise = upload.promise()
 
     promise.then(
       function (data) {
@@ -76,15 +75,14 @@ const Writing = ({accessToken, isLogin, setListRender}) => {
         console.log(data.Location)
       },
       function (err) {
-        console.log(err);
+      console.log(err);
       }
-    );
-  };
+    )
+  }
 
-  const secondImgHandle = (event) => {
-    //input file에서 url만 긁어오는 핸들러
-    const imageFile = event.target.files[0];
-    if (!imageFile) {
+  const secondImgHandle = (event) => { //input file에서 url만 긁어오는 핸들러
+    const imageFile = event.target.files[0]
+    if(!imageFile){
       return setSecondImg(null);
     }
 
@@ -94,9 +92,9 @@ const Writing = ({accessToken, isLogin, setListRender}) => {
         Key: imageFile.name,
         Body: imageFile,
       },
-    });
-
-    const promise = upload.promise();
+    })
+    
+    const promise = upload.promise()
 
     promise.then(
       function (data) {
@@ -104,179 +102,106 @@ const Writing = ({accessToken, isLogin, setListRender}) => {
         console.log(data.Location)
       },
       function (err) {
-        console.log(err);
+      console.log(err);
       }
-    );
-  };
-  //////////////////////이미지 핸들러(위)/////////////////////////////////////
+    )
+  }
+ //////////////////////이미지 핸들러(위)/////////////////////////////////////
 
   const createFeedHandle = () => {
     // 피드테이블에 레코드 생성하는 axios POST 요청(지영)
     // 해당 피드 페이지 or 홈화면으로 Redirect 필요
-    console.log(accessToken, "여기");
 
-    if (
-      title === "" ||
-      firstOpt === "" ||
-      secondOpt === "" ||
-      firstImg === null ||
-      secondImg === null ||
-      content === "" ||
-      isClicked.length === 0
-    ) {
-      setErrorMessage("항목을 모두 입력하세요!🙏");
-    } else {
-      axios.post(
-        "http://ec2-3-34-191-91.ap-northeast-2.compute.amazonaws.com/posting",
-        {
-          title: title,
-          choice_1: firstOpt,
-          choice_2: secondOpt,
-          img_1: firstImg, //url
-          img_2: secondImg, //url
-          contents: content,
-          hashTags: JSON.stringify(isClicked), //배열이니까 JSON?
+    if(title === '' || firstOpt === '' || secondOpt ==='' || firstImg === null || secondImg === null || content === '' || isClicked.length === 0){
+      setErrorMessage('항목을 모두 입력하세요!🙏')
+    } else{
+      console.log('*********************', accessToken)
+      axios.post('http://ec2-3-34-191-91.ap-northeast-2.compute.amazonaws.com/posting', {
+        title: title,
+        choice_1: firstOpt,
+        choice_2: secondOpt,
+        img_1 : firstImg, //url
+        img_2: secondImg, //url
+        contents: content,
+        hashTags: JSON.stringify(isClicked), //배열이니까 JSON?
+      }, { 
+        headers: {
+          authorization: accessToken,
         },
+        "Content-Type": "application/json",
+      })
 
-        {
-          headers: {
-            authorization: accessToken,
-          },
-          "Content-Type": "application/json",
-        }
-      );
-      setListRender(); //홈화면으로 갈 때 바로 useEffect가 호출되어 전체 피드리스트 GET요청이 작동하도록 함.
+      setWriteDone();
     };
-    }
-  };
+  }
 
-
-  if (!isLogin) {
+  if(!isLogin){
     return (
       <section action="" className={styles.container}>
         <div className={styles.loginMessage}>로그인이 필요합니다.</div>
       </section>
-    );
-  } else {
+    )
+  } else{
     return (
       <section className={styles.container}>
+
         <div className={styles.category}>
           <div>제목</div>
-          <input
-            className={styles.textBoxTitle}
-            type="text"
-            value={title}
-            onChange={titleHandle}
-          />
+          <input className={styles.textBoxTitle} type="text" value={title} onChange={titleHandle}/>
         </div>
-
-        <div className={styles.category}>
+        
+        <div className={styles.category}>    
           <div>투표 옵션 1</div>
-          <input
-            className={styles.textBoxOpt}
-            type="text"
-            placeholder="항목명"
-            value={firstOpt}
-            onChange={firstOptHandle}
-          />
+          <input className={styles.textBoxOpt} type="text" placeholder="항목명" value={firstOpt} onChange={firstOptHandle}/>
           <div className={styles.filebox}>
-            <input
-              type="file"
-              id="ex_file"
-              accept="image/*"
-              onChange={firstImgHandle}
-            ></input>
-            {firstImg ? (
-              <img className={styles.fileView} src={firstImg}></img>
-            ) : null}
+            <input type="file" id="ex_file" accept="image/*" onChange={firstImgHandle}></input>
+            {firstImg? <img className={styles.fileView} src={firstImg}></img> : null}
           </div>
         </div>
-
-        <div className={styles.category}>
+    
+    
+        <div className={styles.category}>    
           <div>투표 옵션 2</div>
-          <input
-            className={styles.textBoxOpt}
-            type="text"
-            placeholder="항목명"
-            value={secondOpt}
-            onChange={secondOptHandle}
-          />
+          <input className={styles.textBoxOpt} type="text" placeholder="항목명" value={secondOpt} onChange={secondOptHandle}/>
           <div className={styles.filebox}>
-            <input
-              type="file"
-              id="ex_file"
-              accept="image/*"
-              onChange={secondImgHandle}
-            ></input>
-            {secondImg ? (
-              <img className={styles.fileView} src={secondImg}></img>
-            ) : null}
+            <input type="file" id="ex_file" accept="image/*" onChange={secondImgHandle}></input>
+            {secondImg? <img className={styles.fileView} src={secondImg}></img> : null}
           </div>
         </div>
-
+    
         <div className={styles.category}>
           <div>내용</div>
-          <textarea
-            name="content"
-            id=""
-            cols="30"
-            rows="10"
-            value={content}
-            onChange={contentHandle}
-          ></textarea>
+          <textarea name="content" id="" cols="30" rows="10" value={content} onChange={contentHandle}></textarea>
         </div>
-
+    
         <div className={styles.category}>
-          <div>
-            카테고리<span class={styles.subTitle}>(복수선택가능)</span>
-          </div>
+          <div>카테고리<span class={styles.subTitle}>(복수선택가능)</span></div>
           <div className={styles.tags}>
-            {tags.map((el) => {
-              return (
-                <>
-                  <input type="checkbox" name={el} value={el} />
-                  <label
-                    className={
-                      isClicked.includes(el)
-                        ? styles.hashtagClicked
-                        : styles.hashtag
-                    }
-                    onClick={() => tagHandle(el)}
-                  >
-                    {el}
-                  </label>
-                </>
-              );
+            {tags.map(el =>{ 
+              return <>
+                <input type="checkbox" name={el} value={el}/>
+                <label className={isClicked.includes(el)? styles.hashtagClicked :styles.hashtag} onClick={()=>tagHandle(el)}>{el}</label>
+              </>
             })}
           </div>
         </div>
-
+    
         <div className={styles.submit}>
-          {title === "" ||
-          firstOpt === "" ||
-          secondOpt === "" ||
-          firstImg === null ||
-          secondImg === null ||
-          content === "" ||
-          isClicked.length === 0 ? (
-            <button className={styles.submitBtn} onClick={createFeedHandle}>
-              등록
-            </button>
-          ) : (
-            <Link to="/">
-              <button className={styles.submitBtn} onClick={createFeedHandle}>
-                등록
-              </button>
-            </Link>
-          )}
+          {title === '' || firstOpt === '' || secondOpt ==='' || firstImg === null || secondImg === null || content === '' || isClicked.length === 0 ? 
+            <button className={styles.submitBtn} onClick={createFeedHandle}>등록</button>
+          : 
+          <Link to="/">
+            <button className={styles.submitBtn} onClick={createFeedHandle}>등록</button>
+          </Link>}
           <Link to="/">
             <button className={styles.submitBtn}>취소</button>
           </Link>
         </div>
         <div className={styles.error}>{errorMessage}</div>
+      
       </section>
-    );
-  }
+      )
+    }
 };
 
 export default Writing;
