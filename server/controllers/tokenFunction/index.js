a2fee0ffc0997f3f63885622588facca963b8fd6
Sunryeo <elma9700@gmail.com>
2021-09-01 20:25:08 +0900

    FIX:ProfileChange.js

--- server/controllers/tokenFunction/index.js
+++ server/controllers/tokenFunction/index.js
@@ -3,25 +3,15 @@ const { sign, verify } = require("jsonwebtoken");
 
 module.exports = {
   generateAccessToken: (data) => {
-    return sign(data, process.env.ACCESS_SECRET, { expiresIn: "30s" });
-  },
-  sendAccessToken: (res, accessToken) => {
-    res
-      .cookie("jwt", accessToken, {
-        domain: "localhost:8080",
-        httpOnly: true,
-        sameSite: "lax",
-      })
-      .status(200)
-      .json({ message: "ok" });
+    return sign(data, process.env.ACCESS_SECRET);
   },
   isAuthorized: (req) => {
-    const authorization = req.headers["cookie"];
+    const authorization = req.headers.authorization; /*["cookie"];*/
     if (!authorization) {
       return null;
     }
-    const token = authorization.split(";")[0].split("=")[1]; //토큰 들어오는 것 보고 수정
-
+    const token =
+      authorization; /*.split(";")[0].split("=")[1]; //토큰 들어오는 것 보고 수정*/
     try {
       return verify(token, process.env.ACCESS_SECRET);
     } catch (err) {
