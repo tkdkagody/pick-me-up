35eb09a10f704504449e8eeb7ea63e6714ba2dda
Sunryeo <79203932+Sunryeo@users.noreply.github.com>
2021-09-02 14:15:03 +0900

    Delete user.js

--- server/controllers/user.js
+++
@@ -1,8 +0,0 @@
-const { changeProfile } = require("../controllers/ProfileChange");
-const { getMyPost } = require("../controllers/GetMyPost");
-require("dotenv").config();
-const { Router } = require("express");
-const router = Router();
-
-router.post("/user/profile/:id", changeProfile);
-router.get("/user/posting-list/:id", getMyPost);
